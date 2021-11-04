91385e37b6ea785892acd6b9d9d513f90557b5dd
vic <blovic123@gmail.com>
2021-11-04 21:07:18 +0100

    Fix DejavuTimer optional timing - add separate _decorators_disabled hardcoded option.

--- dejavu/third_party/dejavu_timer.py
+++ dejavu/third_party/dejavu_timer.py
@@ -2,16 +2,19 @@ import codetiming
 
 
 class DejavuTimer(codetiming.Timer):
+    # works on the level of context manager, can be turned on and off dynamically
     timing_enabled = False
+    # disables generation of decorators, hardcoded parameter
+    _decorators_disabled = False
 
     def __init__(self, name, logger=print):
         super().__init__(name=name, logger=logger, text="{name}:\t{:0.4f} seconds\t({milliseconds} ms)")
 
     def __call__(self, func):
-        if DejavuTimer.timing_enabled:
-            return super().__call__(func)
-        else:
+        if DejavuTimer._decorators_disabled:
             return func
+        else:
+            return super().__call__(func)
 
     def __enter__(self):
         if DejavuTimer.timing_enabled:
